4efbac1f675b908368d53c3dd4606c78dde0c064
guyouyin <guyouyin@163.com>
2025-01-14 12:13:10 +0800

    Revert "v4"
    This reverts commit 4fda8a9198492a5e3c5b142cc78941160a986798.

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -10,9 +10,5 @@ fn main() {
 
     println!("Hello, world!");
     let s = format!("Hello, {}!", "world");
-    println!("{}",s);
-
-    println!("Hello, world!");
-    let s = format!("Hello, {}!", "world");
     println!("{}",s)
 }
