02b27753bebf066e20e3558e78fc24bcfe7aaa73
guyouyin <guyouyin@163.com>
2025-01-14 12:09:07 +0800

    v3

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -5,5 +5,10 @@ fn main() {
 
     println!("Hello, world!");
     let s = format!("Hello, {}!", "world");
+    println!("{}",s);
+
+
+    println!("Hello, world!");
+    let s = format!("Hello, {}!", "world");
     println!("{}",s)
 }
